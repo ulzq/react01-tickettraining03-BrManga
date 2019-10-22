439ba62b605d12fb444b3e8e11849ca6276ac8a8
Bora Manga <bora.mng525@gmail.com>
2019-10-22 10:22:40 +0200

    assignment 3

--- src/addPlayer.js
+++ src/addPlayer.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React from "react";
 
 class AddPlayer extends React.Component {
-  state = {name:""};
+  state = { name: "" };
 
-  update = (e) => {
+  update = e => {
     this.setState({
       name: e.target.value
     });
-  }
+  };
 
-  add = ()=> {
-    if ( this.state.name.trim() === "" ){
-      this.props.controller.flashError("Please enter a user name");
-      return; }
-    if ( this.props.playerNames.includes(this.state.name) ){
-      this.props.controller.flashError("Name is taken, choose another one");
-      return; }
-    this.props.addPlayer(this.state.name)
-    this.setState({name:""});
-  }
+  add = () => {
+    if (this.state.name.trim() === "") {
+      this.props.controller("Please enter a user name");
+      return;
+    }
+    if (this.props.playerNames.includes(this.state.name)) {
+      this.props.controller("Name is taken, choose another one");
+      return;
+    }
+    this.props.addPlayer(this.state.name);
+    this.setState({ name: "" });
+  };
 
-  render(){
+  render() {
     return (
       <div className="AddPlayer">
-        <input className="center-relative-h" id="name" onChange={this.update} value={this.state.name} />
-        <button className="center-relative-h" onClick={this.add}>Add Player</button>
-      </div> );
+        <input
+          className="center-relative-h"
+          id="name"
+          onChange={this.update}
+          value={this.state.name}
+        />
+        <button className="center-relative-h" onClick={this.add}>
+          Add Player
+        </button>
+      </div>
+    );
   }
 }
 
